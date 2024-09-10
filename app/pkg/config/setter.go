ebd8c0c0a47ec08606ea184ad6707a86d3053523
hamidrezamahmoodi <h.mahmoodi@utravs.com>
2024-09-10 12:48:17 +0330

    Add default value for yaml file keys using viper

--- app/pkg/config/setter.go
+++ app/pkg/config/setter.go
@@ -9,9 +9,26 @@ import (
 var configurations config.Config
 
 func SetConfig() {
+	// defaults
+	viper.SetDefault("app.name", "Drawo")
+
+	viper.SetDefault("server.host", "localhost")
+	viper.SetDefault("server.port", "8080")
+
+	viper.SetDefault("db.host", "localhost")
+	viper.SetDefault("db.port", "5432")
+
+	viper.SetDefault("auth.issuer", "localhost")
+	viper.SetDefault("auth.audience", "localhost")
+	viper.SetDefault("auth.tokenExpiry", 5)
+	viper.SetDefault("auth.refreshExpiry", 60)
+
+	// set configs
 	viper.SetConfigName(".config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("config")
+
+	// read configs
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatal("Error reading the configs")
 	}
